0aedba66f9c25fcb0af019a93689752e2bd73632
xunchen002 <qwertyui0906@yahoo.com.tw>
2018-09-06 15:15:49 +0800

    Update init.sql

--- mysql_init/init.sql
+++ mysql_init/init.sql
@@ -1,13 +1,6 @@
 create database aws_cert_chatbot;
 use aws_cert_chatbot;
 
-create user 'read_only_user'@'%' IDENTIFIED BY 'iii';
-GRANT SELECT ON aws_cert_chatbot.users TO 'read_only_user'@'%' IDENTIFIED BY 'iii';
-GRANT SELECT ON aws_cert_chatbot.menus TO 'read_only_user'@'%' IDENTIFIED BY 'iii';
-GRANT SELECT ON aws_cert_chatbot.assoc_sa_questions TO 'read_only_user'@'%' IDENTIFIED BY 'iii';
-GRANT SELECT ON aws_cert_chatbot.assoc_dev_questions TO 'read_only_user'@'%' IDENTIFIED BY 'iii';
-GRANT SELECT ON aws_cert_chatbot.assoc_sys_questions TO 'read_only_user'@'%' IDENTIFIED BY 'iii';
-
 CREATE TABLE users (
     user_open_id varchar(255),
     user_nickname varchar(255),
@@ -65,4 +58,10 @@ create table assoc_sys_questions (
 
 insert into assoc_sys_questions values (1,'Which is an operational process performed by AWS for data security?','AES-256 encryption of data stored on any shared storage device (User responsibility)','Decommissioning of storage devices using industry-standard practices','Background virus scans of EBS volumes and EBS snapshots (No virus scan is performed by AWS on User instances)','Replication of data across multiple AWS Regions (AWS does not replicate data across regions unless done by User)',2,'hhhhhh','ghjkkl');
 
+create user 'read_only_user'@'%' IDENTIFIED BY 'iii';
+GRANT SELECT ON aws_cert_chatbot.users TO 'read_only_user'@'%' IDENTIFIED BY 'iii';
+GRANT SELECT ON aws_cert_chatbot.menus TO 'read_only_user'@'%' IDENTIFIED BY 'iii';
+GRANT SELECT ON aws_cert_chatbot.assoc_sa_questions TO 'read_only_user'@'%' IDENTIFIED BY 'iii';
+GRANT SELECT ON aws_cert_chatbot.assoc_dev_questions TO 'read_only_user'@'%' IDENTIFIED BY 'iii';
+GRANT SELECT ON aws_cert_chatbot.assoc_sys_questions TO 'read_only_user'@'%' IDENTIFIED BY 'iii';
 
