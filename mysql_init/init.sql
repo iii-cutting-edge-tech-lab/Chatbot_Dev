90ccc286be903a7568482266d2d5bc68d87554db
iiiiiiii17 <tjcjack1219@gmail.com>
2018-09-06 17:09:51 +0800

    9_6mysql

---
+++ mysql_init/init.sql
@@ -0,0 +1,67 @@
+create database aws_cert_chatbot;
+use aws_cert_chatbot;
+
+CREATE TABLE users (
+    user_open_id varchar(255),
+    user_nickname varchar(255),
+    user_status varchar(255),
+    user_img varchar(255),
+    user_register_date timestamp(6),
+    user_register_menu varchar(255),
+    PRIMARY KEY (user_open_id)
+);
+
+create table menus (
+	menu_id varchar(255),
+	menu_content longtext,
+	PRIMARY KEY (menu_id)
+);
+
+create table assoc_sa_questions (
+	question_id INTEGER(255),
+	question_content TEXT,
+	answer1_content TEXT,
+	answer2_content TEXT,
+	answer3_content TEXT,
+	answer4_content TEXT,
+	true_answer INTEGER(255),
+	true_answer_decribe_content TEXT,
+	external_link VARCHAR(255),
+	PRIMARY KEY (question_id)
+);
+
+create table assoc_dev_questions (
+	question_id INTEGER(255),
+	question_content TEXT,
+	answer1_content TEXT,
+	answer2_content TEXT,
+	answer3_content TEXT,
+	answer4_content TEXT,
+	true_answer INTEGER(255),
+	true_answer_decribe_content TEXT,
+	external_link VARCHAR(255),
+	PRIMARY KEY (question_id)
+);
+
+create table assoc_sys_questions (
+	question_id INTEGER(255),
+	question_content TEXT,
+	answer1_content TEXT,
+	answer2_content TEXT,
+	answer3_content TEXT,
+	answer4_content TEXT,
+	true_answer INTEGER(255),
+	true_answer_decribe_content TEXT,
+	external_link VARCHAR(255),
+	PRIMARY KEY (question_id)
+);
+
+insert into assoc_sys_questions values (1,'Which is an operational process performed by AWS for data security?','AES-256 encryption of data stored on any shared storage device (User responsibility)','Decommissioning of storage devices using industry-standard practices','Background virus scans of EBS volumes and EBS snapshots (No virus scan is performed by AWS on User instances)','Replication of data across multiple AWS Regions (AWS does not replicate data across regions unless done by User)',2,'hhhhhh','ghjkkl');
+
+create user 'read_only_user'@'%' IDENTIFIED BY 'iii';
+GRANT SELECT ON aws_cert_chatbot.users TO 'read_only_user'@'%' IDENTIFIED BY 'iii';
+GRANT SELECT ON aws_cert_chatbot.menus TO 'read_only_user'@'%' IDENTIFIED BY 'iii';
+GRANT SELECT ON aws_cert_chatbot.assoc_sa_questions TO 'read_only_user'@'%' IDENTIFIED BY 'iii';
+GRANT SELECT ON aws_cert_chatbot.assoc_dev_questions TO 'read_only_user'@'%' IDENTIFIED BY 'iii';
+GRANT SELECT ON aws_cert_chatbot.assoc_sys_questions TO 'read_only_user'@'%' IDENTIFIED BY 'iii';
+
