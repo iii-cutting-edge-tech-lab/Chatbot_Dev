8a766aadd0becb18afcbb36762bf977c05790aa4
Wei ren <41813453+k100956@users.noreply.github.com>
2018-09-15 21:07:56 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,14 +4,14 @@ USERNAME=204065533127.dkr.ecr.ap-northeast-1.amazonaws.com
 # image name
 IMAGE=cc102_ecr_test
 docker-compose up -d
-docker images
+#docker images
 #docker-compose build $USERNAME/$IMAGE:latest .
 version=`cat VERSION`
 echo "version: $version"
 version1=`cat VERSION1`
 echo "version: $version1"
-docker tag chatbot_db $USERNAME/$IMAGE:$version
-docker tag jupyter_notebook $USERNAME/$IMAGE:$version1
+docker tag mysql $USERNAME/$IMAGE:$version
+docker tag asoo571314/jupyter-notebook $USERNAME/$IMAGE:$version1
 docker push $USERNAME/$IMAGE:$version
 docker push $USERNAME/$IMAGE:$version1
 
