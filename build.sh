e551d31dd610644cf47863a7ef5b2c507b68dd20
Wei ren <41813453+k100956@users.noreply.github.com>
2018-09-15 21:19:23 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ docker-compose up -d
 #docker-compose build $USERNAME/$IMAGE:latest .
 version=`cat VERSION`
 echo "version: $version"
-version1=`cat VERSION1`
-echo "version: $version1"
-docker tag mysql $USERNAME/$IMAGE:$version
-docker tag asoo571314/jupyter-notebook $USERNAME/$IMAGE:$version1
+#version1=`cat VERSION1`
+#echo "version: $version1"
+docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
+#docker tag asoo571314/jupyter-notebook $USERNAME/$IMAGE:$version1
 docker push $USERNAME/$IMAGE:$version
-docker push $USERNAME/$IMAGE:$version1
+#docker push $USERNAME/$IMAGE:$version1
 
