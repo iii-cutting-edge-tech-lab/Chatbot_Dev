8220cf4ae6d32ccf17cc7d4a8821782c3a9152b4
Wei ren <41813453+k100956@users.noreply.github.com>
2018-09-15 19:45:33 +0800

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,12 @@
+set -ex
+#set registry
+USERNAME=204065533127.dkr.ecr.ap-northeast-1.amazonaws.com
+# image name
+IMAGE=cc102_ecr_test
+docker-compose up -d
+docker build -t $USERNAME/$IMAGE:latest .
+version=`cat VERSION`
+echo "version: $version"
+docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
+docker push $USERNAME/$IMAGE:$version
+
