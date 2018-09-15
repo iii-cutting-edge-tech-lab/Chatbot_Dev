6c358ac35c5e9e0da5d1a87d48ce551779927108
Wei ren <41813453+k100956@users.noreply.github.com>
2018-09-15 20:20:47 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ USERNAME=204065533127.dkr.ecr.ap-northeast-1.amazonaws.com
 # image name
 IMAGE=cc102_ecr_test
 #docker-compose up -d
-docker-compose build  $USERNAME/$IMAGE:latest .
+docker-compose build  #$USERNAME/$IMAGE:latest .
 version=`cat VERSION`
 echo "version: $version"
 docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
