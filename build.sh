6d49019b3739a9665972401ecb6d626c05fcc8b2
Wei ren <41813453+k100956@users.noreply.github.com>
2018-09-16 19:57:47 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,17 +4,18 @@ USERNAME=204065533127.dkr.ecr.ap-northeast-1.amazonaws.com
 # image name
 IMAGE=cc102_ecr_test
 IMAGE_TAG="$USERNAME/$IMAGE:latest"
-docker-compose up -d
+#docker-compose up -d
+docer-compose build
 docker images
 #docker build -t $USERNAME/$IMAGE:latest .
 version=`cat VERSION`
 echo "version: $version"
-version1=`cat VERSION1`
-echo "version: $version1"
-version2=`cat VERSION2`
-echo "version: $version2"
-version3=`cat VERSION3`
-echo "version: $version3"
+#version1=`cat VERSION1`
+#echo "version: $version1"
+#version2=`cat VERSION2`
+#echo "version: $version2"
+#version3=`cat VERSION3`
+#echo "version: $version3"
 docker tag chatbotdev_api:latest $USERNAME/$IMAGE:$version
 docker tag ubuntu:latest $USERNAME/$IMAGE:$version1
 docker tag chatbotdev_db:latest $USERNAME/$IMAGE:$version2
