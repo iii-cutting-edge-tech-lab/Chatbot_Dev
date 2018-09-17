d7e6f8f9698ca8028a6e7cc02f91c2e82b8013e9
Wei ren <41813453+k100956@users.noreply.github.com>
2018-09-17 19:22:16 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ set -ex
 #set registry
 USERNAME=204065533127.dkr.ecr.ap-northeast-1.amazonaws.com
 # image name
-IMAGE=cc102_ecr_test
+IMAGE=cc102_cer_chatbot
 docker build -t $USERNAME/$IMAGE:mysql -f dockerfile/dockerfile-mysql dockerfile/
 docker build -t $USERNAME/$IMAGE:api -f dockerfile/dockerfile-api dockerfile/
 #docker build -t $USERNAME/$IMAGE:redis -f Chatbot_Line/dockerfile-redis Chatbot_Line/
