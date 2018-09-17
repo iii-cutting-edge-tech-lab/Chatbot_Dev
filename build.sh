4db2a18efb7e8a22046158007923ff3e424492a1
Wei ren <41813453+k100956@users.noreply.github.com>
2018-09-17 20:30:08 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ set -ex
 USERNAME=204065533127.dkr.ecr.ap-northeast-1.amazonaws.com
 # image name
 IMAGE=cc102_cer_chatbot
-docker build -t $USERNAME/mysql:1 -f dockerfile/dockerfile-mysql dockerfile/
-docker build -t $USERNAME/api:1 -f dockerfile/dockerfile-api dockerfile/
+docker build -t $USERNAME/$IMAGE:mysql -f dockerfile/dockerfile-mysql dockerfile/
+docker build -t $USERNAME/$IMAGE:api -f dockerfile/dockerfile-api dockerfile/
 #docker build -t $USERNAME/$IMAGE:redis -f Chatbot_Line/dockerfile-redis Chatbot_Line/
 #docker build -t $USERNAME/$IMAGE:jupyter -f Chatbot_Line/dockerfile-jupyter Chatbot_Line/
 #docker build -t $USERNAME/$IMAGE:ngrok -f Chatbot_Line/dockerfile-ngrok Chatbot_Line/
